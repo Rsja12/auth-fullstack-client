177b1d44f0af43e6ca000b97359d8ae43af3cfc8
Rsja12 <asocarras10@gmail.com>
2020-06-26 16:46:06 -0400

    update combine reducers to include form key from redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 
 import authReducer from './authReducer'
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    // key with name of 'form' is required by redux-form
+    form: formReducer
 })
