4726145a48014036a2b2571e5b4bc1ef21f45005
Rsja12 <asocarras10@gmail.com>
2020-06-26 18:19:23 -0400

    create action creator for signup

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { AUTH_USER } from './types'
+
+// thunk allows us to return a function or an obj
+// if we return function it gets called with the dispatch function
+// this gives us ability to return(dispatch) as many actions as we want from a single action creator
+
+// export const signup = ({ email, password }) {
+//     return function(dispatch) {
+//         dispatch({ 
+//             type: AUTH_USER,
+//             payload: { email, password }
+//         })
+//     }
+// } 
+// SAME AS ->
+
+export const signup = formProps => dispatch => {
+    axios.post('http://localhost:3090/signup', formProps)
+}
