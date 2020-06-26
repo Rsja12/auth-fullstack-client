908726a07e6ba3869ef2d058f3d074383509d04e
Rsja12 <asocarras10@gmail.com>
2020-06-26 16:28:13 -0400

    add child Route to App

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import App from './components/App'
 import Welcome from './components/Welcome'
+import SignUp from './components/auth/SignUp'
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
             <Route exact path='/' component={ Welcome } />
+            <Route exact path='/signup' component={ SignUp } />
         </App>
     </BrowserRouter>,
     document.querySelector('#root')
