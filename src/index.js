a18398d6b65cc59993cff4c795badcf023d62d03
Rsja12 <asocarras10@gmail.com>
2020-06-26 17:27:34 -0400

    wire up Thunk

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
 import Welcome from './components/Welcome'
 import SignUp from './components/auth/SignUp'
 import reducers from './reducers'
 
-const store = createStore(reducers, {})
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+)
 
 ReactDOM.render(
     <Provider store={store}>
