def1fe2e889b87d7bb5f5447e5a87f366c98a3ca
Rsja12 <asocarras10@gmail.com>
2020-06-26 16:35:57 -0400

    create combineReducers file and start authReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import { BrowserRouter, Route } from 'react-router-dom'
+
 import App from './components/App'
 import Welcome from './components/Welcome'
 import SignUp from './components/auth/SignUp'
