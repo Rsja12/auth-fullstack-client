6298fa072d1d9c4a60db7138ab16cad4bfbafb06
Rsja12 <asocarras10@gmail.com>
2020-06-26 16:38:21 -0400

    refactor index.js to connect redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
 import Welcome from './components/Welcome'
 import SignUp from './components/auth/SignUp'
+import reducers from './reducers'
+
+const store = createStore(reducers, {})
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route exact path='/' component={ Welcome } />
-            <Route exact path='/signup' component={ SignUp } />
-        </App>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App>
+                <Route exact path='/' component={ Welcome } />
+                <Route exact path='/signup' component={ SignUp } />
+            </App>
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 )
