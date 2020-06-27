00388a33dd5a26c3a03adb5199eda88f325311eb
Rsja12 <asocarras10@gmail.com>
2020-06-27 11:30:29 -0400

    update header to render relevant links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 export class Header extends Component {
+
+    renderLinks() {
+        if (this.props.isSignedIn) {
+            return (
+                <div>
+                    <Link to='/signout'>Sign Out</Link>
+                    <Link to='/feature'>Feature</Link>
+                </div>
+            )
+        } 
+        return (
+            <div>
+                <Link to='/signup'>Sign Up</Link>
+                <Link to='/signin'>Sign In</Link>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div>
                 <Link to='/'>Home</Link>
-                <Link to='/signup'>Sign Up</Link>
-                <Link to='/signin'>Sign In</Link>
-                <Link to='/signout'>Sign Out</Link>
-                <Link to='/feature'>Feature</Link>
+                { this.renderLinks() }
             </div>
         )
     }
 }
 
-export default Header
+const mapStateToProps = state => ({
+    isSignedIn: state.auth.isSignedIn
+})
+
+export default connect(mapStateToProps)(Header)
