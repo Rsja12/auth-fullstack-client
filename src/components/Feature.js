bc19e38ba07214e70d8d32e6ecdcb20fef668836
Rsja12 <asocarras10@gmail.com>
2020-06-27 10:45:27 -0400

    require authentication to render Feature

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import requireAuth from './requireAuth'
+
 export class Feature extends Component {
     render() {
         return (
@@ -10,4 +12,4 @@ export class Feature extends Component {
     }
 }
 
-export default Feature
+export default requireAuth(Feature)
